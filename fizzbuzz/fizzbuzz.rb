8fa66fb69917953db11c58e3bf0dfe3d968bbfcc
michael verdi <michael.v.verdi@gmail.com>
2013-04-17 21:44:09 -0400

    mixed up the wording for fizz and buzz

--- fizzbuzz/fizzbuzz.rb
+++ fizzbuzz/fizzbuzz.rb
@@ -1,10 +1,10 @@
 (0..50).each do |num|
   if num % 3 == 0 and num % 5 == 0
-    puts "Fizz"
-  elsif num % 3 == 0
-    puts "Buzz"
-  elsif num % 5 == 0
     puts "FizzBuzz"
+  elsif num % 3 == 0
+    puts "Fizz"
+  elsif num % 5 == 0
+    puts "Buzz"
   else
     puts num
   end
